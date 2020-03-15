7f4374f458b2e92f556ddaf8db6dd2443090051e
syh7 <sjoerd0707@live.nl>
2020-03-15 14:15:02 +0100

    Return application name on "/"

--- src/main/java/koalatree/controllers/ModuleController.java
+++ src/main/java/koalatree/controllers/ModuleController.java
@@ -1,7 +1,6 @@
 package koalatree.controllers;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,8 +11,7 @@ public class ModuleController {
     private String appName;
 
     @GetMapping("/")
-    public String homePage(Model model) {
-        model.addAttribute("appName", appName);
-        return "home";
+    public String homePage() {
+        return appName;
     }
 }
