8194331b11ae006eb16211104f351ef078cfd091
syh7 <sjoerd0707@live.nl>
2020-03-15 21:44:30 +0100

    Add null check for message

--- src/main/java/koalatree/serialization/TransactionDeserializer.java
+++ src/main/java/koalatree/serialization/TransactionDeserializer.java
@@ -1,16 +1,16 @@
 package koalatree.serialization;
 
-import java.io.IOException;
-import java.math.BigDecimal;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
 import koalatree.domain.Category;
 import koalatree.domain.Transaction;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class TransactionDeserializer extends StdDeserializer<Transaction> {
 
@@ -27,14 +27,16 @@ public class TransactionDeserializer extends StdDeserializer<Transaction> {
     @Override
     public Transaction deserialize(JsonParser parser, DeserializationContext deserializationContext) throws IOException {
         JsonNode node = parser.getCodec().readTree(parser);
-        return Transaction.builder()
+        Transaction.TransactionBuilder builder = Transaction.builder()
                 .amountTotal(BigDecimal.valueOf(Double.parseDouble(node.get("amountTotal").asText())))
                 .amountSjoerd(BigDecimal.valueOf(Double.parseDouble(node.get("amountSjoerd").asText())))
                 .amountLoes(BigDecimal.valueOf(Double.parseDouble(node.get("amountLoes").asText())))
                 .amountAll(BigDecimal.valueOf(Double.parseDouble(node.get("amountAll").asText())))
                 .category(Category.valueOfDutchName(node.get("category").asText()))
-                .message(node.get("message").asText())
-                .date(LocalDate.parse(node.get("date").asText(), formatter))
-                .build();
+                .date(LocalDate.parse(node.get("date").asText(), formatter));
+        if (node.has("message")) {
+            builder.message(node.get("message").asText());
+        }
+        return builder.build();
     }
 }
